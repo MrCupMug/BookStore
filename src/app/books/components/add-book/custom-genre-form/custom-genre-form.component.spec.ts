af355b493945719c69d08f6555d812387b08916a
Vladislav <vladek.bagdasarov27@gmail.com>
2021-09-13 11:19:01 +0300

    updating angular

--- src/app/books/components/add-book/custom-genre-form/custom-genre-form.component.spec.ts
+++ src/app/books/components/add-book/custom-genre-form/custom-genre-form.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CustomGenreFormComponent } from './custom-genre-form.component';
 
@@ -6,7 +6,7 @@ describe('CustomGenreFormComponent', () => {
   let component: CustomGenreFormComponent;
   let fixture: ComponentFixture<CustomGenreFormComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CustomGenreFormComponent ]
     })
