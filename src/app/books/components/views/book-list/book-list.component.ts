22500560e38d60b058a3ed44a5b074d1d9c370ec
Vladislav <vladek.bagdasarov27@gmail.com>
2021-10-21 18:06:15 +0300

    books containers are partly done

--- src/app/books/components/views/book-list/book-list.component.ts
+++ src/app/books/components/views/book-list/book-list.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
+import { Observable } from 'rxjs';
+
+import { IBook } from '../../../../books/interfaces/books.interface';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.scss']
 })
-export class BookListComponent implements OnInit {
+export class BookListComponent {
+
+  @Input()
+  public books: Observable<IBook[]>;
+
+  @Input()
+  public pageSizeOptions: Observable<number>;
+
+  @Output()
+  public bookInfo = new EventEmitter<IBook>();
+
+  @Output()
+  public filter = new EventEmitter<void>();
+
+  @Output()
+  public pagination = new EventEmitter<any>();
+
+  public booksMeta = {
+    length: 100,
+    pageSize: 10,
+    pageIndex: 1,
+  };
 
   constructor() { }
 
-  ngOnInit(): void {
+  public setBookInfo(book: IBook): void {
+    this.bookInfo.emit(book);
+  }
+
+  public openFilter(): void {
+    this.filter.emit();
+  }
+
+  public setPagination(pagination: PageEvent): void {
+    this.pagination.emit(pagination);
   }
 
 }
